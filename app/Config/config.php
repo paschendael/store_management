5d76d66bcb6928bf1e6a86b24924fb7b49f5be09
Miaw <Miaw@Miaw-PC>
2014-12-21 02:53:26 -0500

    commit a jour

---
+++ app/Config/config.php
@@ -0,0 +1,26 @@
+<?php
+
+$config = array(
+	'Application' => array(
+		'name' 	  => 'StoreManagement',
+		'version' => 'v0.7',
+		'status'  => true,
+		'maintenance' => 0
+	),
+	'Meta' => array(
+		'title' 	  => 'Store Management',
+		'description' => '{{description}}',
+		'keywords' 	  => '{{keywords}}',
+	),
+	
+	'Google' => array(
+		'analytics'  => '{{analytics}}',
+	),
+	
+	'Email' => array(
+		'from_email' => array('{{from_name}}' => '{{from_email}}'),
+		'contact_mail' => array('{{contact_name}}' => '{{contact_mail}}')
+	)
+);
+
+?>
